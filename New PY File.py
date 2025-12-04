80bf0520147f1ffeb7192f7b294793b9739eb8f1
plsstop123 <nguyentienhung22707@gmail.com>
2025-12-04 09:52:04 +0700

    13

--- New PY File.py
+++ New PY File.py
@@ -1,4 +1,11 @@
-x = "Python "
-y = "is "
-z = "awesome"
-print(x + y + z)
+x = float(input("Nhap so thu nhat:"))
+y = float(input("Nhap so thu hai:"))
+def multiply(x, y) :
+  """Function calculate multiply of 2 numbers"""
+  return x * y
+
+# Gọi hàm sử dụng và truyền tham số
+mul = multiply(x, y)
+
+# In kết quả => 40
+print(mul)
